f56345c2bb0b00cb835274c9b68ad9bd116a4a82
Danil Novyh <danil.novyh@yandex.ru>
2025-12-06 12:01:26 +0400

    Small changes in urls

--- urls.py
+++ urls.py
@@ -1,15 +1,15 @@
 class Urls:
-    # Основная страница
-    BASE_URL = "https://stellarburgers.nomoreparties.site/"
+    # Основная страница BASE_URL
+    url_main_paige = "https://stellarburgers.nomoreparties.site/"
 
-    # Ссылка на страницу входа
-    LOGIN_PAGE = "https://stellarburgers.nomoreparties.site/login"
+    # Ссылка на страницу входа LOGIN_PAGE
+    url_login = "https://stellarburgers.nomoreparties.site/login"
 
-    # Ссылка формы Личный кабинет -> Профиль
+    # Ссылка формы Личный кабинет -> Профиль CONSTRUCTOR_PAGE???
     url_profile = "https://stellarburgers.nomoreparties.site/account/profile"
 
-    # Ссылка на страницу регистрации
+    # Ссылка на страницу регистрации REGISTER_PAGE
     url_register = "https://stellarburgers.nomoreparties.site/register"
 
-    # Ссылка на страницу забыл пароль
+    # Ссылка на страницу забыл пароль RECOVERY_PAGE
     url_forgot_password = "https://stellarburgers.nomoreparties.site/forgot-password"
